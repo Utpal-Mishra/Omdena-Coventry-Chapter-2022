41cb12a14c6b32bb28148898b7687011bc748fdd
Utpal Mishra <utpalmishra21official@gmail.com>
2021-11-28 06:06:53 +0000

    Building

---
+++ sections/aboutme.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>    
+        <meta charset = "UTF-8"> 
+        <title>About Me/title>
+        <link rel = "stylesheet" type = "text/css" href = "aboutme.css">\</head>    
+</head>
+
+<body>
+
+    <h2 data-text = "name">UTPAL MISHRA</h2>
+
+</body>
+
+</html>
